0c2b420956fb769dc7627073b8dc4f155b19c083
pp144 <pedromanpereira@gmail.com>
2022-09-18 00:16:41 +0100

    phone

---
+++ phone_num.hs
@@ -0,0 +1,26 @@
+
+
+
+number :: String -> Maybe String
+number xs = error "You need to implement this function."
+
+
+
+
+
+main :: IO ()
+main = do
+{- comentario multiline
+-}
+--comentario oneline
+-- compilar  ghc -o hw hw.hs
+--correr ./hw
+-- ctrl + space run in VS
+-- ctr + S to save in VS 
+
+
+
+
+let num=6
+print (aliquotsum num)
+print(classify num)
