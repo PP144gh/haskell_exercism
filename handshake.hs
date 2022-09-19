773a4d53d44e1a5db05b8c18b0761fc55414e06d
pp144 <pedromanpereira@gmail.com>
2022-09-19 19:17:11 +0100

    handshake exercise

---
+++ handshake.hs
@@ -0,0 +1,23 @@
+handshake :: Int -> [String]
+handshake n = error "You need to implement this function."
+
+
+
+
+main :: IO ()
+main = do
+{- comentario multiline
+-}
+--comentario oneline
+-- compilar  ghc -o hw hw.hs
+--correr ./hw
+-- ctrl + space run in VS
+-- ctr + S to save in VS 
+
+
+
+
+let num=6
+print (aliquotsum num)
+print(classify num)
+
