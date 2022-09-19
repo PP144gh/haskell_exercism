496f9eb56f0be3d2c9dc19fa64ce08639b01c1cf
pp144 <pedromanpereira@gmail.com>
2022-09-19 19:15:04 +0100

    phone number exercise

--- phone_num.hs
+++ phone_num.hs
@@ -1,9 +1,20 @@
 
+import Data.Char 
+import Data.List
+import Data.Maybe
+import Data.Foldable
+
 
 
 number :: String -> Maybe String
-number xs = error "You need to implement this function."
-
+number [] = Nothing
+number xs 
+    | head ns == '1' = number (drop 1 ns)
+    | head ns == '0' || head ns == '1' || ns !! 3 == '0' ||  ns !! 3 == '1' = Nothing 
+    | length ns == 10 = Just ns
+    | otherwise = Nothing
+    where ns= filter (\x -> x `notElem` badchars) (map toLower xs)
+          badchars="abcdefghijklmnopqrstuvxywz" ++ ['(',')','-','.',',',' ','@',':','!','?','+',';','_','*']
 
 
 
@@ -21,6 +32,5 @@ main = do
 
 
 
-let num=6
-print (aliquotsum num)
-print(classify num)
+let num = "1 (023) 456-7890"
+print(number num)
