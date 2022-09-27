9c58d01a68b4bab31326f2d88e2547ac9964815e
pp144 <pedromanpereira@gmail.com>
2022-09-27 16:55:37 +0100

    robot

--- robot_sim.hs
+++ robot_sim.hs
@@ -1,19 +1,38 @@
+
 data Bearing = North
              | East
              | South
              | West
              deriving (Eq, Show)
 
-data Robot = Dummy
+
+-- data is like a class and inside it are "objects", that why North, East etc are called constructors.
+
+
+type Pos = (Integer,Integer)
+
+
+-- class robot with object mkrobot with properties Bearing and Pos
+
+data Robot = MkRobot { direction :: Bearing, coords :: Pos}
+    deriving Show
+
+-- this is called record syntax. it better than MkRobot direction coordinates because one can refer to the properties by their name and not by their order in the constructor.
+-- AND accessor functions are automatically defined. see bearing and coordinates.
+
+mkRobot :: Bearing -> Pos -> Robot
+mkRobot direction coordinates = MkRobot direction coordinates
+
 
 bearing :: Robot -> Bearing
-bearing robot = error "You need to implement this function."
+bearing robot = direction robot
 
-coordinates :: Robot -> (Integer, Integer)
-coordinates robot = error "You need to implement this function."
 
-mkRobot :: Bearing -> (Integer, Integer) -> Robot
-mkRobot direction coordinates = error "You need to implement this function."
+
+coordinates :: Robot -> Pos
+coordinates robot = coords robot
+
+
 
 move :: Robot -> String -> Robot
 move robot instructions = error "You need to implement this function."
@@ -34,7 +53,9 @@ main = do
 
 
 
-let num=6
-print (aliquotsum num)
-print(classify num)
-
+let dir = North
+let pos =(0,0)
+let robot=mkRobot dir pos
+print(robot)
+print(bearing robot)
+print(coordinates robot)
