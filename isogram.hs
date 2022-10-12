d5e2fa6cd7b089bfb3758a3827ba26fa5a67694e
pp144 <pedromanpereira@gmail.com>
2022-10-12 14:37:27 +0100

    isogram

--- isogram.hs
+++ isogram.hs
@@ -1,7 +1,18 @@
+import qualified Data.Char
+import Data.Char (toLower)
+
 isIsogram :: String -> Bool
-isIsogram = error "You need to implement this function!"
+isIsogram word = not . any (>1) $ [count x word2 | x <- word2 ]
+     where word2 = map toLower word
 
 
+count :: Char -> [Char] -> Int
+count x xs = length [x' | x' <- xs, x == x' && x /= ' ' && x /= '-']
+-- counts how many x chars appear in xs. ignores white spaces and -
+
+
+test :: [Char] -> [Int]
+test word2= [count x word2 | x <- word2 ]
 
 main :: IO ()
 main = do
@@ -13,6 +24,8 @@ main = do
 -- ctrl + space run in VS
 -- ctr + S to save in VS 
 
-
-let num=9
-print(num)
+--let tup = zip [0 | x <- ['a'..'z']] ['a'..'z']
+let word = "ola-adeus"
+print(isIsogram word)
+print(test word)
+print(count '-' word)
