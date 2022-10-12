2fd4c830ae52beca9cd2d3b4f5cadfbdad54a46f
pp144 <pedromanpereira@gmail.com>
2022-10-12 14:55:12 +0100

    isogram cleaner

---
+++ isbn.hs
@@ -0,0 +1,23 @@
+isbn :: String -> Bool
+isbn = error "You need to implement this function."
+
+
+
+
+
+
+main :: IO ()
+main = do
+{- comentario multiline
+-}
+--comentario oneline
+-- compilar  ghc -o hw hw.hs
+--correr ./hw
+-- ctrl + space run in VS
+-- ctr + S to save in VS 
+
+
+
+
+let num=9
+print(num)
--- isogram.hs
+++ isogram.hs
@@ -14,6 +14,14 @@ count x xs = length [x' | x' <- xs, x == x' && x /= ' ' && x /= '-']
 test :: [Char] -> [Int]
 test word2= [count x word2 | x <- word2 ]
 
+test2 :: [Char] -> [(Char, Int)]
+test2 word = zip ['a'..'z'] [count x word | x <- ['a'..'z'] ]
+
+-- or 
+isIsogram' :: [Char] -> Bool
+isIsogram' word  = null . filter (\(_,b) -> b>1) $ zip ['a'..'z'] [count x $ map toLower word | x <- ['a'..'z'] ]
+
+
 main :: IO ()
 main = do
 {- comentario multiline
@@ -25,7 +33,9 @@ main = do
 -- ctr + S to save in VS 
 
 --let tup = zip [0 | x <- ['a'..'z']] ['a'..'z']
+
 let word = "ola-adeus"
+print(test2 word)
 print(isIsogram word)
 print(test word)
 print(count '-' word)
