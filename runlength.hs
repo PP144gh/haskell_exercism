7bd50c483ff7e914d27197dfb7ba4a3fe6cf17c1
pp144 <pedromanpereira@gmail.com>
2022-10-22 11:07:03 +0200

    runlength almost there

--- runlength.hs
+++ runlength.hs
@@ -1,38 +1,24 @@
 decode :: String -> String
-decode encodedText = error "You need to implement this function."
-
-encode :: String -> String
-encode text = error "You need to implement this function."
-
-
-rmdups :: Eq a => [a] -> [a]
-rmdups [] = []
-rmdups (x : xs) = x : rmdups (filter (/= x) xs)
+decode encodedText = replicate repeating (head (tail encodedText))
+    where repeating = read [head encodedText]
 
 positions :: Eq a => a -> [a] -> [Int]
 positions x xs = [i | (x', i) <- zip xs [0 ..], x == x']
 
 
-test :: Eq a => [a] -> [[Int]]
-test word2= [positions x word2 | x <- word2 ]
-
-test2 code = zip (rmdups code) (rmdups $ test code)
---test2 code = [(x,y) | x <- (rmdups code), y <- (rmdups $ test code)]
-
---func ::[(Char, [Int])] -> Int -> (Char, Int)
-
+fil :: (Num a, Ord a) => [a] -> a -> a
 fil [] n = n
 fil (x:xs) n
     | x == n = fil xs (n+1)
     | x>n = n
     | otherwise = 0
+--counts how many times one letter repeats successively.
 
-
-func :: [Char] -> [Char]
-func [] = []
-func code =head code: show (repeating) ++ func (drop repeating code)
-    where repeating = fil (positions (head code) code) 0
-
+encode :: String -> String
+encode [] = []
+encode text = show repeating ++ head text:encode (drop repeating text)
+    where repeating = fil (positions (head text) text) 0
+-- head code is 'a' and show repeating is 'b':[]. head code is a pure char and show repeating is a list with one entry that is a char, a string with only one element.
 main :: IO ()
 main = do
 {- comentario multiline
@@ -46,12 +32,10 @@ main = do
 
 
 let code = "WWWWWWWWWWWWBBZZZW"
-let repeating = fil (positions 'W' code) 0
-print(rmdups code)
-print(positions 'W' code)
-print(fil (positions 'W' code) 0)
-print(drop repeating code)
-print(func code)
+let dcode= encode code
+print(encode code)
+print (dcode)
+print(decode dcode)
 --print(rmdups $ test code)
 --print(test2 code)
 --print(func (test2 code) 0 )
