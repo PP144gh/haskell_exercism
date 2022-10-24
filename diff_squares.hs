5ba8ce1178a37264a595fc1f25730d5e05b57b59
pp144 <pedromanpereira@gmail.com>
2022-10-24 15:24:33 +0200

    diff of squares

---
+++ diff_squares.hs
@@ -0,0 +1,30 @@
+difference :: Integral a => a -> a
+difference n= squareOfSum n - sumOfSquares n 
+
+squareOfSum :: Integral a => a -> a
+squareOfSum n = (foldr (+) 0 [1..n])^2
+
+sumOfSquares :: Integral a => a -> a
+sumOfSquares n = foldr ((+) . (^2)) 0 [1..n]
+
+
+
+
+
+main :: IO ()
+main = do
+{- comentario multiline
+-}
+--comentario oneline
+-- compilar  ghc -o hw hw.hs
+--correr ./hw
+-- ctrl + space run in VS
+-- ctr + S to save in VS 
+
+
+
+
+let num=3
+print(sumOfSquares num)
+print(squareOfSum num)
+print(difference num)
