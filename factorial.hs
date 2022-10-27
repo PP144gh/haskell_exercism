d69cd05b71cf9b3226fef64eb84d9179a24ce0c0
Pedro M. F. Pereira <pedromanpereira@gmail.com>
2022-10-27 22:55:25 +0100

    Update factorial.hs
    new factorial forms

--- factorial.hs
+++ factorial.hs
@@ -3,7 +3,9 @@
 
 factorial :: Integer ->  Integer
 factorial 0 = 1
-factorial n = n * factorial (n-1)
+--factorial n = n * factorial (n-1)
+factorial n = foldr (*) 1 [n,n-1..1]
+--factorial n = product [n,n-1..1]
 
 ---------------------
 
