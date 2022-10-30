47a1736fd05a7c34e84e924358b1e08a844befc0
pp144 <pedromanpereira@gmail.com>
2022-10-30 19:57:48 +0000

    diamond done

---
+++ diamond.hs
@@ -0,0 +1,42 @@
+
+import Data.Char
+
+diamond :: Char -> Maybe [String]
+diamond char = if num >= 65 && num<=90 then Just (maker 'A' char ++ (tail . reverse) (maker 'A' char)) else Nothing
+    where num = ord char
+
+
+
+maker :: Char -> Char -> [String]
+maker chari charf
+    | numi == 0 = (spacel numf ++ [chari] ++ spacel numf) : maker (chr (numi +66)) charf
+    | numi <= numf =  (spacel (numf - numi) ++ [chari] ++ spacel (2*numi -1) ++ [chari] ++ spacel (numf -numi)) : maker (chr (numi +66)) charf
+    | otherwise = []
+ where
+    spacel num = replicate num ' '
+    numi = mod (ord chari) 65
+    numf = mod (ord charf) 65
+
+
+
+printdiamond :: Maybe [String] -> String
+printdiamond (Just []) = []
+printdiamond list = (head . (\(Just i) -> i) $ list ) ++ "\n" ++ printdiamond (Just (tail . (\(Just i) -> i) $ list))
+
+main :: IO ()
+main = do
+{- comentario multiline
+-}
+--comentario oneline
+-- compilar  ghc -o hw hw.hs
+--correr ./h`
+-- ctrl + space run in VS
+-- ctr + S to save in VS 
+
+
+
+
+
+let char = 'Z'
+print (maker 'A' char)
+putStr(printdiamond $ diamond char)
